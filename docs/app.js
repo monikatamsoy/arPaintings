24b9d9e228feeb32bf671417fbf9feb83135bac4
monika-tamsoy <monika.tamsoy@hcl.com>
2022-01-15 22:59:22 +0530

    rotate model with gesture

--- docs/app.js
+++ docs/app.js
@@ -154,7 +154,7 @@ class App{
                 self.startQuaternion = self.chair.quaternion.clone();
             }else{
                 self.chair.quaternion.copy( self.startQuaternion );
-                self.chair.rotation = ev.theta;
+                self.chair.rotation.y = ev.theta;
             }
         });
         this.renderer.setAnimationLoop( this.render.bind(this) );
